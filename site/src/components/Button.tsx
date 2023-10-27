b5b9171c1b9827e1c916ef56901da6a19c5fb141
edwin-shdw <edwin.krivoseev@gmx.de>
2023-10-27 12:38:04 +0200

    fix(docs): replace example button link

--- site/src/components/Button.tsx
+++ site/src/components/Button.tsx
@@ -2,24 +2,32 @@ import * as React from 'react';
 
 export default function Button({
   children,
+  className,
   variant,
   disabled,
   as = 'button',
-  href,
+  ...props
 }: {
   children: React.ReactElement,
+  className: string,
   variant?: string,
   disabled?: boolean,
   as?: React.ElementType,
   href?: string,
+  props: React.HTMLProps<any>
 }): React.ReactElement {
   const Tag = as;
   const btnVariant = variant ? `dc-btn-${variant}` : null;
   let classString = btnVariant ? `dc-btn ${btnVariant}` : 'dc-btn';
-  classString += (disabled && as === 'a') ? ' dc-btn-disabled' : '';
+  if(disabled && (as === 'a')) classString += ' dc-btn-disabled';
+  if(className) classString += ` ${className}`;
 
   return(
-    <Tag className={classString} disabled={disabled} href={href}>
+    <Tag
+      className={classString}
+      disabled={disabled}
+      {...props}
+    >
       {children}
     </Tag>
   );
