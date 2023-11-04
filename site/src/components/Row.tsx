ad57619e8e7024c831f6fc858582edced35d68a4
edwin-shdw <edwin.krivoseev@gmx.de>
2023-11-04 15:17:13 +0100

    docs: add documentation for focus-rings

--- site/src/components/Row.tsx
+++ site/src/components/Row.tsx
@@ -2,14 +2,16 @@ import * as React from 'react';
 
 export default function Row({
   children,
+  gapWidth = '0.5rem',
 }: {
   children: React.ReactNode,
+  gapWidth?: number | string,
 }): React.ReactElement {
   return(
     <div
       style={{
         display: 'flex',
-        gap: '0.5rem',
+        gap: gapWidth,
       }}>
       {children}
     </div>
