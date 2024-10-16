59f7971d3f092845e40be615332a187ab24efb9b
edwin-shdw <edwin.shdw@gmail.com>
2024-10-16 22:10:35 +0200

    refactor!: drop deprecated rgba in favor of rgb

--- site/src/components/Mention.tsx
+++ site/src/components/Mention.tsx
@@ -7,7 +7,7 @@ function hexToRgb(hex: string): string {
   const g = (bigint >> 8) & 255;
   const b = bigint & 255;
 
-  return r + ", " + g + ", " + b;
+  return `${r} ${g} ${b}`;
 }
 
 export default function Mention({
@@ -20,10 +20,10 @@ export default function Mention({
   color?: string,
 }): React.ReactElement {
   const colorValue = (role && color) ? hexToRgb(color) : 'var(--dc-blurple-rgb)';
-  const colorStyle = { '--dc-mention-color': colorValue } as React.CSSProperties;
+  const colorStyle = {'--dc-mention-color': colorValue} as React.CSSProperties;
   const mentionClass = role ? 'dc-mention dc-mention-role' : 'dc-mention';
 
-  return(
+  return (
     <span
       className={mentionClass}
       style={role ? colorStyle : null}
